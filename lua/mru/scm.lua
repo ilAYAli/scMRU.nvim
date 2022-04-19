fdb7974240e67dc16f135e2e6b37e32def51462c
Petter Wahlman <pwahlman@cisco.com>
2022-04-19 07:54:11 +0000

    Improve scm.get_project_root
    don't overwrite filename if supplied.

--- lua/mru/scm.lua
+++ lua/mru/scm.lua
@@ -11,10 +11,15 @@ end
 function M.get_project_root(filename)
     local cwd = vim.loop.cwd()
 
-    if filename ~= nil then
+    if filename == nil then
         filename = vim.api.nvim_buf_get_name(0)
+    end
+
+    if filename ~= nil then
         local dir = get_parent(filename)
-        vim.api.nvim_set_current_dir(dir)
+        if dir ~= nil then
+            vim.api.nvim_set_current_dir(dir)
+        end
     end
 
     local f = assert(io.popen("git rev-parse --show-toplevel 2>/dev/null"))
